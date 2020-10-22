a8ea224cd1296592822fc41e7dd2f6efa486239b
Tomas <firbedatomasf@gmail.com>
2020-10-22 02:29:08 -0300

    comit

--- detail.php
+++ detail.php
@@ -1,6 +1,7 @@
 <?php
 // SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
+//require __DIR__ .  '/vendor/autoload.php';
+require('./vendor/autoload.php');
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
@@ -49,9 +50,9 @@ $preference->payment_methods = array(
     "installments" => 6
   );
   $preference->back_urls = array(
-    "success" => "success.php",
-    "failure" => "error.php",
-    "pending" => "pendiente.php"
+    "success" => "http://localhost:8888/mercadopago/success.php",
+    "failure" => "http://localhost:8888/mercadopago/error.php",
+    "pending" => "http://localhost:8888/mercadopago/pendiente.php"
 );
 $preference->external_reference = "firbedatomasf@gmail.com";
 
